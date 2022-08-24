fe545d9cea5aae617038ec9a2b49ad52afa83d70
hayatoyu <f19870421@gmail.com>
2022-08-24 11:06:55 +0800

    20220824-1

---
+++ test/flutter_application_1/lib/google_contact_model.dart
@@ -0,0 +1,207 @@
+// To parse this JSON data, do
+//
+//     final googleContacts = googleContactsFromJson(jsonString);
+
+import 'dart:convert';
+
+GoogleContacts googleContactsFromJson(String str) => GoogleContacts.fromJson(json.decode(str));
+
+String googleContactsToJson(GoogleContacts data) => json.encode(data.toJson());
+
+class GoogleContacts {
+  GoogleContacts({
+    required this.connections,
+    required this.nextPageToken,
+    required this.totalPeople,
+    required this.totalItems,
+  });
+
+  List<Connection> connections;
+  String nextPageToken;
+  int totalPeople;
+  int totalItems;
+
+  factory GoogleContacts.fromJson(Map<String, dynamic> json) => GoogleContacts(
+    connections: List<Connection>.from(json["connections"].map((x) => Connection.fromJson(x))),
+    nextPageToken: json["nextPageToken"],
+    totalPeople: json["totalPeople"],
+    totalItems: json["totalItems"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "connections": List<dynamic>.from(connections.map((x) => x.toJson())),
+    "nextPageToken": nextPageToken,
+    "totalPeople": totalPeople,
+    "totalItems": totalItems,
+  };
+}
+
+class Connection {
+  Connection({
+    required this.resourceName,
+    required this.etag,
+    this.names,
+    this.phoneNumbers,
+  });
+
+  String resourceName;
+  String etag;
+  List<Name>? names;
+  List<PhoneNumber>? phoneNumbers;
+
+  factory Connection.fromJson(Map<String, dynamic> json) => Connection(
+    resourceName: json["resourceName"],
+    etag: json["etag"],
+    names: json["names"] == null ? null : List<Name>.from(json["names"].map((x) => Name.fromJson(x))),
+    phoneNumbers: json["phoneNumbers"] == null ? null : List<PhoneNumber>.from(json["phoneNumbers"].map((x) => PhoneNumber.fromJson(x))),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "resourceName": resourceName,
+    "etag": etag,
+    "names": names == null ? null : List<dynamic>.from(names!.map((x) => x.toJson())),
+    "phoneNumbers": phoneNumbers == null ? null : List<dynamic>.from(phoneNumbers!.map((x) => x.toJson())),
+  };
+}
+
+class Name {
+  Name({
+    required this.metadata,
+    required this.displayName,
+    required this.familyName,
+    required this.givenName,
+    required this.displayNameLastFirst,
+    required this.unstructuredName,
+    required this.middleName,
+  });
+
+  Metadata metadata;
+  String displayName;
+  String familyName;
+  String givenName;
+  String displayNameLastFirst;
+  String unstructuredName;
+  String middleName;
+
+  factory Name.fromJson(Map<String, dynamic> json) => Name(
+    metadata: Metadata.fromJson(json["metadata"]),
+    displayName: json["displayName"],
+    familyName: json["familyName"] ?? "",
+    givenName: json["givenName"],
+    displayNameLastFirst: json["displayNameLastFirst"],
+    unstructuredName: json["unstructuredName"],
+    middleName: json["middleName"] ?? "",
+  );
+
+  Map<String, dynamic> toJson() => {
+    "metadata": metadata.toJson(),
+    "displayName": displayName,
+    "familyName": familyName,
+    "givenName": givenName,
+    "displayNameLastFirst": displayNameLastFirst,
+    "unstructuredName": unstructuredName,
+    "middleName": middleName,
+  };
+}
+
+class Metadata {
+  Metadata({
+    required this.primary,
+    required this.source,
+  });
+
+  bool primary;
+  Source source;
+
+  factory Metadata.fromJson(Map<String, dynamic> json) => Metadata(
+    primary: json["primary"] ?? "",
+    source: Source.fromJson(json["source"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "primary": primary,
+    "source": source.toJson(),
+  };
+}
+
+class Source {
+  Source({
+    this.type,
+    this.id,
+  });
+
+  SourceType? type;
+  String? id;
+
+  factory Source.fromJson(Map<String, dynamic> json) => Source(
+    type: sourceTypeValues.map[json["type"]]!,
+    id: json["id"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "type": sourceTypeValues.reverse![type],
+    "id": id,
+  };
+}
+
+enum SourceType { CONTACT }
+
+final sourceTypeValues = EnumValues({
+  "CONTACT": SourceType.CONTACT
+});
+
+class PhoneNumber {
+  PhoneNumber({
+    this.metadata,
+    this.value,
+    this.canonicalForm,
+    this.type,
+    this.formattedType,
+  });
+
+  Metadata? metadata;
+  String? value;
+  String? canonicalForm;
+  PhoneNumberType? type;
+  FormattedType? formattedType;
+
+  factory PhoneNumber.fromJson(Map<String, dynamic> json) => PhoneNumber(
+    metadata: Metadata.fromJson(json["metadata"]),
+    value: json["value"],
+    canonicalForm: json["canonicalForm"] ?? "",
+    type: phoneNumberTypeValues.map[json["type"]]!,
+    formattedType: formattedTypeValues.map[json["formattedType"]]!,
+  );
+
+  Map<String, dynamic> toJson() => {
+    "metadata": metadata!.toJson(),
+    "value": value,
+    "canonicalForm": canonicalForm ?? "",
+    "type": phoneNumberTypeValues.reverse![type],
+    "formattedType": formattedTypeValues.reverse![formattedType],
+  };
+}
+
+enum FormattedType { MOBILE }
+
+final formattedTypeValues = EnumValues({
+  "Mobile": FormattedType.MOBILE
+});
+
+enum PhoneNumberType { MOBILE }
+
+final phoneNumberTypeValues = EnumValues({
+  "mobile": PhoneNumberType.MOBILE
+});
+
+class EnumValues<T> {
+  Map<String, T> map;
+  Map<T, String>? reverseMap;
+
+  EnumValues(this.map);
+
+  Map<T, String>? get reverse {
+    reverseMap ??= map.map((k, v) => MapEntry(v, k));
+    return reverseMap;
+  }
+}
--- test/flutter_application_1/lib/loginscreen.dart
+++ test/flutter_application_1/lib/loginscreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/google_contact_model.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
-
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -11,35 +11,80 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
   late GoogleSignIn _googleSignIn;
   late GoogleSignInAccount _currentUser;
+  GoogleContacts? contacts;
+  bool IsLoading = false;
+
+  Future getUserContact() async {
+    const host = "people.googleapis.com";
+    const endPoint =
+        "/v1/people/me/connections?personFields=names,emailAddresses";
+    final header = await _currentUser.authHeaders;
+
+    setState(() {
+      IsLoading = true;
+    });
+    print("loading contacts...");
+
+    final response = await http.get(
+        Uri(
+            scheme: 'https',
+            host: host,
+            path: '/v1/people/me/connections',
+            queryParameters: {
+              'personFields': 'names,emailAddresses',
+              'pageSize': '30',
+              'sources': [
+                'READ_SOURCE_TYPE_PROFILE',
+                'READ_SOURCE_TYPE_CONTACT'
+              ]
+            }),
+        headers: header);
+    setState(() {
+      IsLoading = false;
+    });
+
+    if (response.statusCode == 200) {
+      print("Google API works");
+      setState(() {
+        contacts = googleContactsFromJson(response.body);
+      });
+    } else {
+      print("Google API error");
+    }
+    print(response.body);
+  }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    _googleSignIn = GoogleSignIn(
-      scopes: [
-        "https://www.googleapis.com/auth/contacts.readonly",        
-      ]
-    );
-    _googleSignIn.onCurrentUserChanged.listen(
-      (user) { 
-        setState(() {
-          _currentUser = user!;
-        });
-        print(_currentUser.displayName);
-      }
-    );
+    _googleSignIn = GoogleSignIn(scopes: [
+      "https://www.googleapis.com/auth/contacts.readonly",
+    ]);
+    _googleSignIn.onCurrentUserChanged.listen((user) {
+      setState(() {
+        if (user != null) {
+          _currentUser = user;
+          getUserContact();
+        }
+      });
+      user != null ? print(_currentUser.displayName) : null;
+    });
   }
 
   Widget LoginWidget() {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        const Text("See contacts saved in your gmail",style: TextStyle(fontSize: 24),),
-        const SizedBox(height: 16,),
+        const Text(
+          "See contacts saved in your gmail",
+          style: TextStyle(fontSize: 24),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
         FloatingActionButton(
           backgroundColor: Colors.grey[900],
           onPressed: () async {
@@ -52,10 +97,28 @@ class _LoginScreenState extends State<LoginScreen> {
           child: const Text(
             "Google Sign in",
             style: TextStyle(color: Colors.white),
-          ),          
+          ),
         )
       ],
     );
+  }
+
+  Widget getContactWidget() {
+    return ListView.separated(
+        itemBuilder: (context, index) {
+          final currentContact = contacts!.connections[index];
+          if(currentContact.names != null) {
+            return ListTile(
+              title: Text(currentContact.names!.first.displayName),
+            );
+          } else {
+            return const ListTile(
+              title: Text("No Name"),
+            );
+          }
+        },
+        separatorBuilder: (context, index) => const Divider(),
+        itemCount: contacts!.connections.length);
   }
 
   @override
@@ -67,7 +130,13 @@ class _LoginScreenState extends State<LoginScreen> {
       body: Container(
         width: double.infinity,
         padding: const EdgeInsets.all(16),
-        child: LoginWidget(),
+        child: IsLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : contacts != null && _currentUser != null
+                ? getContactWidget()
+                : LoginWidget(),
       ),
     );
   }
